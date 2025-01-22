dadebdcd686decc119ead1e01260e340d561230d
codewithgracie <emmanuelgrace411@gmail.com>
2025-01-23 00:06:50 +0100

    bankai

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,14 @@ module.exports = {
         'nav-default': '#5A4A79',
         'nav-hover': '#FF6B6B',
         'nav-active': '#FFFFFF',
-        'subtitles': '#6A4E74'
+        'subtitles': '#6A4E74',
+        'dark-purple': '#000235',
+        'btn-primary': '#7e7ee3',
+        'heading-color': '#abd9d9'
       },
       fontFamily: {
         'body': ['inter', 'sans-serif'],
+        'Poppins': 'Poppins',
       },
     },
   },
